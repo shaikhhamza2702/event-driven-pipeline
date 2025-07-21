9a76e3430cf6f5f66d04d1622d1d14ddc0f6297d
hamza <hamza123@gmail.com>
2025-07-21 15:00:46 +0530

    Fix: use unique S3 bucket names

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "input_bucket" {
-  bucket = "event-driven-pipeline-input"
+  bucket = "event-driven-pipeline-input-shaik-12345"
 }
 
 resource "aws_s3_bucket" "output_bucket" {
-  bucket = "event-driven-pipeline-output"
+  bucket = "event-driven-pipeline-output-shaik-12345"
 }
